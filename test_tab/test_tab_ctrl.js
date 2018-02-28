122d92d9c0f362041eea637fe1fda55e8057fb2b
joo <iamjoo@gmail.com>
2018-02-27 21:32:54 -0800

    actually wrote the method that inserts emoji

--- test_tab/test_tab_ctrl.js
+++ test_tab/test_tab_ctrl.js
@@ -14,9 +14,11 @@ class TestTabCtrl {
 
     this.amount = 12000;
 
+    this.lastRange;
+
     this.init_();
 
-    $scope.$on(constants.Event.INSERT_EMOJI, this.insertEmoji_);
+    $scope.$on(constants.Event.INSERT_EMOJI, this.insertEmoji_.bind(this));
   }
 
   open() {
@@ -34,7 +36,46 @@ class TestTabCtrl {
   onFocus() {
   }
 
+  onMessageEntryBlur() {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    range.setStart(selection.focusNode, range.startOffset);
+    range.setEnd(selection.focusNode, range.endOffset);
+
+    this.lastRange = range;
+  }
+
   insertEmoji_() {
+    // This is a node
+    const messageEntry = document.getElementById('gw-message-entry');
+    let range;
+
+    if (this.lastRange) {
+      range = this.lastRange;
+    } else {
+      range = new Range();
+      range.setStart(messageEntry, 0);
+      range.setEnd(messageEntry, 0);
+    }
+
+    range.deleteContents();
+
+    // Create a new img element and insert it where the cursor used to be
+    const newImg = document.createElement('img');
+    newImg.setAttribute('class', 'gw-emoji-icon');
+    newImg.setAttribute(
+        'src',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Emoji_u263a.svg/128px-Emoji_u263a.svg.png');
+    range.insertNode(newImg);
+
+    const newRange = new Range();
+    newRange.setStart(messageEntry, range.startOffset + 1);
+    newRange.setEnd(messageEntry, range.startOffset + 1);
+    this.lastRange = newRange;
+  }
+
+  replaceTextWithEmoji_() {
+    // This is a node
     const messageEntry = document.getElementById('gw-message-entry');
 
     // Move focus back to the message entry box
@@ -51,6 +92,7 @@ class TestTabCtrl {
     const range = selection.getRangeAt(0);
     range.setStart(selection.focusNode, index);
     range.setEnd(selection.focusNode, index + 4);
+    console.warn(range);
     range.deleteContents();
 
     // Create a new img element and insert it where "test" used to be
